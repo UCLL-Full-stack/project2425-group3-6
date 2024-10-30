8ca86971e9d5772eaefea9cdfae9f778c1b05f50
Maya Martens <maya.martens@student.ucll.be>
2024-10-30 17:45:24 +0100

    login page design done

---
+++ front-end/components/header.tsx
@@ -0,0 +1,17 @@
+import Head from 'next/head';
+import Link from 'next/link';
+
+const Header: React.FC = () => {
+  return (
+    <>
+   <header className="relative z-50">
+      <h1 className='text-6xl py-7 ml-20 oooh-baby-regular text-white z-50'>Flavor Forge</h1>
+    </header>
+
+    
+    </>
+    
+  );
+};
+
+export default Header;
